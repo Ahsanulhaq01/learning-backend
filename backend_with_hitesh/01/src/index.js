a51b7b4084f6acf1e1a967be657a6e3ac4376c73
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-10-02 15:24:34 +0500

    ma

--- backend_with_hitesh/01/src/index.js
+++ backend_with_hitesh/01/src/index.js
@@ -1,11 +1,20 @@
 // require('dotenv').config({path :'./env'}); 
 // but this line is breaking our consistency because of importing it through require while other are imported as import statment to solve this problem
-
+import { app } from './app.js';
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
 dotenv.config({ path: './.env',quiet: true})
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 3000 , ()=>{
+        console.log(`server is running at port ${process.env.PORT}`)
+    });
+    app.on('error',(error)=>{
+        console.error("Error" , error)
+    })
+}).catch((error)=>{
+    console.log('mongo db is getting error while connecting to db !')
+});
 /*
 import express from 'express';
 const app = express();
