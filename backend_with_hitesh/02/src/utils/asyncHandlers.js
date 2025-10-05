047a2b7fac66efa71b4103338c0644edd5d0c66b
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-10-05 19:23:37 +0500

    add asyncHandlers utility for centralized async error handling

---
+++ backend_with_hitesh/02/src/utils/asyncHandlers.js
@@ -0,0 +1,24 @@
+const asyncHandlers = (requestHandler)=>{
+    async (req,res,next)=>{
+        Promise.resolve(requestHandler(req , res ,next)).catch((err)=>{
+            next(err);
+        })
+    }
+   
+}
+
+export {asyncHandlers}
+
+
+// const asyncHandlers = (requesthandler)=>
+//     async (req ,res , next)=>{
+//         try {
+//             await requesthandler(req , res, next);
+
+//         } catch (error) {
+//             res.status(err.code || 500).json({
+//                 success : false,
+//                 message : err.message,
+//             })
+//         }
+//     }
