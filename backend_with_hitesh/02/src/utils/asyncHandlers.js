4274f331f6c63cce361dc63f923bb3c523e60184
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-10-06 23:50:08 +0500

    write return to forward the function result

--- backend_with_hitesh/02/src/utils/asyncHandlers.js
+++ backend_with_hitesh/02/src/utils/asyncHandlers.js
@@ -1,5 +1,5 @@
 const asyncHandlers = (requestHandler)=>{
-    async (req,res,next)=>{
+   return async (req,res,next)=>{
         Promise.resolve(requestHandler(req , res ,next)).catch((err)=>{
             next(err);
         })
